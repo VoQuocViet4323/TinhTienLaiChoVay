8bc6f9ce73b2ed5112b0c93bd25b8a10e0d689a2
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-06 22:38:18 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double money = 1.0;
+        int month = 1;
+        double laisuat = 1.0;
+
+        Console.WriteLine("Nhap so tien gui: ");
+        money = Double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nhap so thang gui tien: ");
+        month = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nhap lai suat: ");
+        laisuat = Double.Parse(Console.ReadLine());
+
+        double TongTienLai = 0;
+        for (int i = 0; i < month; i++)
+        {
+            TongTienLai = money * (laisuat / 100) / 12 * 3;
+        }
+        Console.WriteLine("Total of interset: " + TongTienLai);
+    }
+}
